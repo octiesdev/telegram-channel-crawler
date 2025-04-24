91306a6083cee1cd11cde7499d523965aeccd5ea
НовыйИмя <Email>
2025-04-24 20:45:10 +0200

    add: config.js for env and paths

--- bot.js
+++ bot.js
@@ -6,13 +6,11 @@ const { CONFIG } = require("./config.js");
 const bot = new TelegramBot(CONFIG.TELEGRAM_BOT_TOKEN, { polling: true });
 
 // Проверка: только админ может использовать бота
-bot.use((msg, next) => {
-  if (msg.chat.id.toString() === CONFIG.ADMIN_CHAT_ID) {
-    next();
-  } else {
-    bot.sendMessage(msg.chat.id, "⛔️ У тебя нет доступа к этому боту.");
-  }
-});
+bot.on("message", (msg) => {
+    if (msg.chat.id.toString() !== CONFIG.ADMIN_CHAT_ID) {
+      bot.sendMessage(msg.chat.id, "⛔️ У тебя нет доступа к этому боту.");
+    }
+  });
 
 // /start
 bot.onText(/\/start/, (msg) => {
